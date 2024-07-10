08fba1485fc133166b7ef9bc4b21d4769d57ab50
Johnny <139769556+Sjossa@users.noreply.github.com>
2024-07-10 15:44:13 +0200

    10/04/24 APREM

--- Language/main/bootstrap/index.php
+++ Language/main/bootstrap/index.php
@@ -7,21 +7,18 @@
     <title>Bootstrap</title>
 </head>
 <body>
-<header>
-<?php
-include_once "../../header.php";
-?>
-</header>
+    <header>
+        <?php include_once "../../header.php"; ?>
+    </header>
 
-<main class="container-fluid">
-    <div class="row">
+    <main class="container-fluid fs-5">
         <h1 class="text-center">Les different modification de texte</h1>
-        <div class="col-6 mt-3">
-            <h3 class="text-muted">Les couleur</h3>
-            En utilisant Bootstrap,nous pouvons modifier la couleur du texte et le fond grâce aux préfixes 'text' et 'bg' suivis de la couleur voulue.
-            nous devons d'abord connaître les couleur que bootstrap a implémenter les voici:
-            <div class="col"> 
-                <ul>
+        <div class="row">
+            <div class="col-5 border border-black ms-2">
+                <h3 class="text-muted">Les couleur</h3>
+                <p>En utilisant Bootstrap, il est possible de modifier la couleur du texte et du fond en utilisant les préfixes 'text' et 'bg', puis la couleur voulue.Avant toute chose, il est essentiel de comprendre les couleurs que Bootstrap a mises en place.</p>
+                <p>Voici les différents couleur que bootstrap propose:</p>
+                <ul class=" border border-danger">
                     <li>Pour la couleur bleue, on utilise le suffixe <em class="text-primary">primary</em></li>
                     <li>Pour la couleur grise claire, on utilise le suffixe <em class="text-secondary">secondary</em></li>
                     <li>Pour la couleur verte, on utilise le suffixe <em class="text-success">success</em></li>
@@ -34,85 +31,61 @@ include_once "../../header.php";
                     <li>Pour la couleur grise foncée, on utilise le suffixe <em class="text-muted">muted</em></li>
                     <li>Pour une couleur de lien, on utilise le suffixe <em class="text-link">link</em></li>
                 </ul>
+                <p>Par exemple, en utilisant la classe 'text-primary', <span class="text-primary">le texte est en bleu</span>.</p>
+                <p>Pour modifier le fond, on utilise le préfixe 'bg' au lieu de 'text', ce qui donne 'bg-primary' : <span class="bg-primary text-white">le fond sera en bleu</span>.</p>
             </div>
-            <div class="col">
-                Par exemple, en utilisant la classe 'text-primary', le texte sera en bleu :
-                <div class="col text-primary">le texte en bleu</div>
+            <div class="col col-3">
+                <div id="editor" class="w-100 h-100">entre votre code ici...</div>
             </div>
-            <div class="col">
-                Pour modifier le fond, on utilise le préfixe 'bg' au lieu de 'text', ce qui donne 'bg-primary' :
-                <div class="col bg-primary text-white">le fond sera en bleu</div>
+            <div class="col-3">
+                <button onclick="runCode()">Exécuter le Code</button>
+                <h4>Zone de texte</h4>
+                <div id="output" class="w-100 h-100">Le résultat s'affichera ici...</div>
             </div>
-            <div class="col">a votre droite vous pourrez vous essayer a ces different préfixe</div>
         </div>
-        <div class="col-6 mt-1">
-            <h3>Pour vous entraînez :</h3>
-            <div class="row">
-            <div class="col col-6">
-                <div id="editor" style="height: 400px; width: 100%;"></div>
-                    <button onclick="runCode()">Exécuter le Code</button>
-                </div>
-                    <div class="col col-6"><pre id="output"></pre></div>
-            </div>
-
-
-        </div>
-    </div>
-
-<div class="row mt-3">
-    <h3 class="text-secondary">les Tailles</h3>
-    <div class="col col-6">
+        <hr>
+        <div class="row">
+            <h3 class="text-secondary">les Tailles</h3>
+    <div class="col col-6   border border-black">
     <p>Après avoir vu comment changer les couleurs du texte et du fond, nous allons maintenant voir comment modifier la taille  de votre texte.
     en premier lieu ils nous faut connaître leur préfixe qui  sont '<em class="fw-bold">fs</em> pour la taille du texte et 'display' pour la taille des titre  et '<em class="fw-bold">fw</em>' pour le poids.</p>
     <div class="col bd-example">
         <p>les suffixe qui accompagne les different préfixe de taille sont les nombre de '1' à '5':</p>
         <div class="row">
-        <div class="col col-6">
+        <div class="col col-4">
         <ul>
-            <li class="fs-1">ceci est un texte avec un texte de taille fs-1</em></li>
-            <li class="fs-2">ceci est un texte avec un texte de taille fs-2</em></li>
-            <li class="fs-3">ceci est un texte avec un texte de taille fs-3</em></li>
-            <li class="fs-4">ceci est un texte avec un texte de taille fs-4</em></li>
-            <li class="fs-5">ceci est un texte avec un texte de taille fs-5</em></li>
+            <li >ceci est un texte avec un mot de taille :<span class="fs-1"> fs-1</span></li>
+            <li>ceci est un texte avec un mot de taille : <span class="fs-2"> fs-2</span></li>
+            <li>ceci est un texte avec un mot de taille : <span class="fs-3">fs-3</span> </li>
+            <li>ceci est un texte avec un mot de taille : <span class="fs-4">fs-4</span> </li>
+            <li>ceci est un texte avec un mot de taille : <span class="fs-5">fs-5</span> </li>
+            <li> titre de taille <span class="display-1">display-1</span></li>
+            <li> titre de taille <span class="display-2">display-2</span></li>
+            <li> titre de taille <span class="display-3">display-3</span></li>
+            <li> titre de taille <span class="display-4">display-4</span></li>
+            <li> titre de taille <span class="display-5">display-5</span></li>
         </ul>
-            </div>
-            <div class="col col-6 fs-5">
-                <ul>
-            <li> titre de taille <em class="display-1">display-1</em></li>
-            <li> titre de taille <em class="display-2">display-2</em></li>
-            <li> titre de taille <em class="display-3">display-3</em></li>
-            <li> titre de taille <em class="display-4">display-4</em></li>
-            <li> titre de taille <em class="display-5">display-5</em></li>
-        </ul>
-        </div>
+        
         </div>
     </div>
     </div>
-    <div class="col col-6">
+            
 
-    </div>
-
-</div>
-
-
-   
-</main>
+        </div>
+    </main>
 
 
 <script>
-                var editor = ace.edit("editor");
-                editor.setTheme("ace/theme/dracula");
-                editor.session.setMode("ace/mode/html");
-
-                    function runCode() {
-                        var code = editor.getValue();
-                        try {
+     var editor = ace.edit("editor");
+        editor.setTheme("ace/theme/dracula");
+        editor.session.setMode("ace/mode/html");
+            function runCode() {
+                var code = editor.getValue();
+                    try {
                         document.getElementById('output').innerHTML = code;
-                            } catch (error) {
+                        } catch (error) {
                         document.getElementById('output').innerText = error;
-                        }
-                }
-
-            </script>
+                        }}
+</script>
 </body>
 </html>
