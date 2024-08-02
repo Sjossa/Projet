aa6a0fc74bc870577b56b05a0ba50390a6dd0fd3
Johnny <139769556+Sjossa@users.noreply.github.com>
2024-08-02 11:14:42 +0200

    02/08/2024

--- Astrologie/header.php
+++ Astrologie/header.php
@@ -1,23 +1,17 @@
 
-<?php define('BASE_PATH', '/projet/Language/');?>
-<?php define('BOOTSTRAP_PATH', '/projet/Language/main/bootstrap/');?>
+<?php define('BASE_PATH', '/projet/astrologie/');?>
 <?php define('NODE_PATH', '/projet');?> 
 <link rel="stylesheet" href="<?php echo NODE_PATH; ?>/node_modules/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="asset/css/astro.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js"></script>
-<body>
-  <div class="container-fluide fs-3">
-<nav class="navbar navbar-expand-lg bg-warning">
+  <div class="fs-3">
+<nav class="navbar navbar-expand-lg bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="<?php echo BASE_PATH; ?>">🏠</a>
+    <a class="navbar-brand fs-1" href="<?php echo BASE_PATH; ?>">🏠</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link " href="<?php echo BOOTSTRAP_PATH;?>">Bootstrap</a>
-      </div>
-    </div>
   </div>
 </nav>
 </div>
-</body>
+
---
+++ Astrologie/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>astrologie-planète-signe-du-zodiaque</title>
+</head>
+<body>
+<header><?php include_once"header.php"?></header>
+
+<main>
+    <div class="container-fluid"></div>
+</main>
+
+
+   
+</body>
+</html>
